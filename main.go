613e9bd884a2c1d48fe3ae58cac548a0287a9c25
Thauan <dev.thauan.rodrigues@gmail.com>
2024-12-23 00:37:31 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/ThauanRodriguesDev/jobs-register/router"
+import (
+	"github.com/ThauanRodriguesDev/jobs-register/config"
+	"github.com/ThauanRodriguesDev/jobs-register/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+
+	//Initialize Configs
+
+	err := config.Init()
+
+	if err != nil {
+		logger.ErrorF("Config Initializate Error: %v\n", err)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
